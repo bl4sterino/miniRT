532b9009b37d5897affcd8545f121d265b84bffd
Paul Berne <berne.d.paul@gmail.com>
2026-03-20 11:57:36 +0100

    PARSING SEGFAULT BAND-AID

--- src/parsing/parsing_material.c
+++ src/parsing/parsing_material.c
@@ -6,7 +6,7 @@
 /*   By: pberne <pberne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:56:36 by pberne            #+#    #+#             */
-/*   Updated: 2026/03/04 18:21:42 by pberne           ###   ########.fr       */
+/*   Updated: 2026/03/18 14:16:51 by pberne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ t_list	*ft_get_parser_material(int id)
 	d = (t_parsing_data){p_float_0_255_to_0_1, offsetof(t_material, color.z),
 		0};
 	ft_lstadd_back(&lst, ft_lstnew_gc_id(ft_get_pdata(id, d), id));
-	d = (t_parsing_data){p_float_try_or_0, offsetof(t_material, diffusion), 0};
+	d = (t_parsing_data){p_float, offsetof(t_material, diffusion), 0};
 	ft_lstadd_back(&lst, ft_lstnew_gc_id(ft_get_pdata(id, d), id));
-	d = (t_parsing_data){p_float_try_or_0, offsetof(t_material, reflectiveness),
+	d = (t_parsing_data){p_float, offsetof(t_material, reflectiveness),
 		0};
 	ft_lstadd_back(&lst, ft_lstnew_gc_id(ft_get_pdata(id, d), id));
-	d = (t_parsing_data){p_float_try_or_0, offsetof(t_material, emission), 0};
+	d = (t_parsing_data){p_float, offsetof(t_material, emission), 0};
 	ft_lstadd_back(&lst, ft_lstnew_gc_id(ft_get_pdata(id, d), id));
-	d = (t_parsing_data){p_float_try_or_0, offsetof(t_material, refraction), 0};
+	d = (t_parsing_data){p_float, offsetof(t_material, refraction), 0};
 	ft_lstadd_back(&lst, ft_lstnew_gc_id(ft_get_pdata(id, d), id));
 	return (lst);
 }
